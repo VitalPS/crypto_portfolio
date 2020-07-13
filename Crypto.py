7c979ea66b13b74e6c61be3b0e7864e37dc95b8a
Patrik da Silva Vital <patrikdasilvavital@gmail.com>
2020-07-12 21:51:47 -0300

    - "Add coin" button added - "Update coin" button added - "Delete coin" button added - File menu added    - "Clear portfolio data" button added to file menu    - "Close application" button added to file menu

--- Crypto.py
+++ Crypto.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import Menu
 import requests
 import json
 import sqlite3
@@ -16,8 +18,35 @@ cursor_obj.execute("CREATE TABLE IF NOT EXISTS coin(id INTEGER PRIMARY KEY, symb
 connection.commit()
 
 
+def restart():
+    for cell in pycripto.winfo_children():
+        cell.destroy()
+
+    top_bar()
+    app_header()
+    my_portfolio()
+
+
+def top_bar():
+    def clear_portfolio():
+        cursor_obj.execute("DELETE FROM coin")
+        connection.commit()
+        restart()
+        messagebox.showinfo("Portfolio notification", "All data was excluded from your portfolio successfully!")
+
+    def close_app():
+        pycripto.destroy()
+
+    bar = Menu(pycripto)
+    file_item = Menu(bar)
+    file_item.add_command(label='Clear portfolio', command=clear_portfolio)
+    file_item.add_command(label='Close application', command=close_app)
+    bar.add_cascade(label="File", menu=file_item)
+    pycripto.config(menu=bar)
+
+
 def my_portfolio():
-    api_request = requests.get('YOU PUT YOUR API KEY HERE!')  # fetch API data from internet
+    api_request = requests.get('ADD YOUR API KEY HERE!')  # fetch API data from internet
 
     api = json.loads(api_request.content)  # store API data in a variable
 
@@ -29,6 +58,24 @@ def my_portfolio():
             return 'green'
         else:
             return 'red'
+
+    def insert_coin():
+        cursor_obj.execute("INSERT INTO coin(symbol, amount, price) VALUES(?, ?, ?)", (coin_symbol_txt.get(), coins_owned_txt.get(), price_txt.get()))
+        connection.commit()
+        restart()
+        messagebox.showinfo("Portfolio notification", "Coin added to your portfolio successfully!")
+
+    def update_coin():
+        cursor_obj.execute("UPDATE coin SET symbol=?, amount=?, price=? WHERE id=?", (update_symbol_txt.get(), update_coins_owned_txt.get(), update_price_txt.get(), update_id_txt.get()))
+        connection.commit()
+        restart()
+        messagebox.showinfo("Portfolio notification", "Coin updated successfully!")
+
+    def delete_coin():
+        cursor_obj.execute("DELETE FROM coin WHERE id=?", (delete_id.get()))
+        connection.commit()
+        restart()
+        messagebox.showinfo("Portfolio notification", "Coin deleted from your portfolio successfully!")
 
     general_pl = 0
     total_current_value = 0
@@ -50,7 +97,7 @@ def my_portfolio():
                 portfolio_id_f = Label(pycripto, text=dict_pos[0], bg='#E8DAEF', fg='black', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
                 portfolio_id_f.grid(row=counter, column=0, sticky=N + S + E + W)
 
-                coin_name_f = Label(pycripto, text=api['data'][web_pos]['name'], bg='#E8DAEF', fg='black', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
+                coin_name_f = Label(pycripto, text=api['data'][web_pos]['symbol'], bg='#E8DAEF', fg='black', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
                 coin_name_f.grid(row=counter, column=1, sticky=N + S + E + W)
 
                 price_f = Label(pycripto, text='$ {:.4f}'.format(api['data'][web_pos]['quote']['USD']['price']), bg='#E8DAEF', fg='black', font='Lato 12', padx='5', pady='5', borderwidth=2, relief='groove')
@@ -73,6 +120,43 @@ def my_portfolio():
 
                 counter += 1  # increase row number
 
+    # insert data
+    coin_symbol_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    coin_symbol_txt.grid(row=counter+1, column=1)
+
+    price_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    price_txt.grid(row=counter+1, column=2)
+
+    coins_owned_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    coins_owned_txt.grid(row=counter+1, column=3)
+
+    add_coin_button = Button(pycripto, text='ADD COIN', command=insert_coin, bg='#4B0082', fg='white', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
+    add_coin_button.grid(row=counter+1, column= 4)
+
+    # update data
+    update_id_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    update_id_txt.grid(row=counter + 2, column=0)
+
+    update_symbol_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    update_symbol_txt.grid(row=counter+2, column=1)
+
+    update_price_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    update_price_txt.grid(row=counter+2, column=2)
+
+    update_coins_owned_txt = Entry(pycripto, borderwidth=2, relief='groove')
+    update_coins_owned_txt.grid(row=counter+2, column=3)
+
+    update_coin_button = Button(pycripto, text=' UPDATE  ', command=update_coin, bg='#4B0082', fg='white', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
+    update_coin_button.grid(row=counter+2, column=4)
+
+    # delete data
+    delete_id = Entry(pycripto, borderwidth=2, relief='groove')
+    delete_id.grid(row=counter+3, column=0)
+
+    delete_coin_button = Button(pycripto, text='  DELETE  ', command=delete_coin, bg='#4B0082', fg='white', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
+    delete_coin_button.grid(row=counter+3, column=4)
+
+
     total_invest_f = Label(pycripto, text='$ {:.2f}'.format(total_invest), bg='#E8DAEF', fg='black', font='Lato 12', borderwidth=2, relief='groove')
     total_invest_f.grid(row=counter, column=4, sticky=N + S + E + W)
 
@@ -84,7 +168,7 @@ def my_portfolio():
 
     api = ''  # resets api information
 
-    refresh = Button(pycripto, text='REFRESH!', command=my_portfolio, bg='#4B0082', fg='white', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
+    refresh = Button(pycripto, text='REFRESH!', command=restart, bg='#4B0082', fg='white', font='Lato 12 bold', padx='5', pady='5', borderwidth=2, relief='groove')
     refresh.grid(row=counter + 1, column=7, sticky=N + S + E + W)
 
 
@@ -114,6 +198,7 @@ def app_header():
     pl_total.grid(row=0, column=7, sticky=N+S+E+W)
 
 
+top_bar()
 app_header()
 my_portfolio()
 
